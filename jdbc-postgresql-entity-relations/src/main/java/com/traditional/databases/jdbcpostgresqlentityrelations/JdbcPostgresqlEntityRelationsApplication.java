24526a3dae3831e66bdacec8af345799f823ba96
samantrs <https://samantrs@git.rabobank.nl>
2023-03-25 17:15:23 +0100

    first commit

---
+++ jdbc-postgresql-entity-relations/src/main/java/com/traditional/databases/jdbcpostgresqlentityrelations/JdbcPostgresqlEntityRelationsApplication.java
@@ -0,0 +1,13 @@
+package com.traditional.databases.jdbcpostgresqlentityrelations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcPostgresqlEntityRelationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcPostgresqlEntityRelationsApplication.class, args);
+	}
+
+}
---
+++ jdbc-postgresql-entity-relations/src/test/java/com/traditional/databases/jdbcpostgresqlentityrelations/JdbcPostgresqlEntityRelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traditional.databases.jdbcpostgresqlentityrelations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcPostgresqlEntityRelationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
