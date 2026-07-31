860a41c7615d4e5ea00620e5064fddd87ae989e1
Sashank <sashanka.samantray@rabobank.nl>
2026-07-31 20:37:58 +0200

    added a new module

---
+++ jdbc-one-to-many-uni-directional-relation/src/main/java/com/paypulse/platform/jdbconetomanyunidirectionalrelation/JdbcOneToManyUniDirectionalRelationApplication.java
@@ -0,0 +1,13 @@
+package com.paypulse.platform.jdbconetomanyunidirectionalrelation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcOneToManyUniDirectionalRelationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcOneToManyUniDirectionalRelationApplication.class, args);
+    }
+
+}
---
+++ jdbc-one-to-many-uni-directional-relation/src/test/java/com/paypulse/platform/jdbconetomanyunidirectionalrelation/JdbcOneToManyUniDirectionalRelationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paypulse.platform.jdbconetomanyunidirectionalrelation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcOneToManyUniDirectionalRelationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
